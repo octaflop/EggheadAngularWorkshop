b856d60a466d23c69bfe34a0c48264a5174bb500
Faris Chebib <faris@zaneprep.com>
2015-03-04 14:02:18 -0700

    refactor to people

--- public/http/app.js
+++ public/http/app.js
@@ -5,13 +5,31 @@ angular.module('workshop', [])
   .controller('WorkshopCtrl', function WorkshopCtrl($http){
     var workshop = this;
 
-    workshop.games = [];
+    workshop.people = [];
 
     // $http.get("$%)(*") // returns an error
-    $http.get("/games")
-      .then(function success(result) {
-        workshop.games = result.data;
-      }, function error(error) {
-        console.log(error);
-      });  // .then promises to get games
+    workshop.loadGames = function loadGames () {
+      $http.get("/people")
+        .then(function success(result) {
+          workshop.people = result.data;
+        }, function error(error) {
+          console.log(error);
+        });  // .then promises to get games
+    };
+
+    // CRUD stuff
+    workshop.loadGames();
+
+    workshop.removeGame = function removeGame(personID) {
+      var deletion_url = "/people/" + personID;
+      console.log("DELETION" + deletion_url);
+      $http.delete(deletion_url)
+        .then(function success(result){
+          console.log(result);
+          workshop.loadGames();
+        }, function error(error){
+          console.log(error);
+        });
+    };
+
   });
